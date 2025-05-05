4c4befd6d653ca367716e713c6c8ef495d41d50f
satyamworks247 <satyamworks247@gmail.com>
2025-05-05 15:16:42 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId= 144553
+let accountEmail="Satyamrohini@gmail.com"
+var accountPassword="12345"
+accountCity="Delhi"
+
+// accountId=2 // NOT ALLOWED 
+
+accountEmail="hc@hc.com"
+accountPassword="212121"
+accountCity="Bengaluru"
+console.log(accountId);
+ 
+console.table([accountId,accountEmail,accountPassword,accountCity])
+/*
+PREFER NOT TO USE VAR BECAUSE OF ISSUE IN  BLOCK SCOPE & FUNCTIONAL SCOPE 
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"//treat all JS code as newer version
+// alert(3+3)// we are using nodejs,not browser
+console.log(3+3)
+console.log("Satyam Shrivastava")
+
+// Code Redeability must be of top priority
+let name = "Satyam"
+let age="20"
+let isLoggedIn= false
+// Primitive data types
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null =>standalone value
+// undefined =>
+// symbol => uniquesness(used in figma tools/react) 
+
+
+// object
+
+console.log(typeof undefined);// undefined
+console.log(typeof null);// object (javascript ki khami)
