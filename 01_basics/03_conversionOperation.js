b28887ec932bf34aa1dd4bb553da93008fb2e692
satyamworks247 <satyamworks247@gmail.com>
2025-05-05 16:04:36 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33";
+
+console.log(typeof score);           // string
+console.log(typeof(score));          // string
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);   // number
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.lof(booleanIsLoggedIn);
+
+ // jb aap 1 likhte hai toh true 1=>true; 0=>false
+ //" "=> false
+ //" hitesh or koi bhi naam"=" true"
+
+ let someNumber = 33
+
+ let stringNumber = String(someNumber);
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
