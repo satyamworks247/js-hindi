e46bc0251bc98943eef6e274ccb339cd77db063e
satyamworks247 <satyamworks247@gmail.com>
2025-05-05 16:50:42 +0000

    operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,27 @@
+let value = 3;
+let negValue = -value
+console.log(negValue);
+// operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+
+let str1 = "hello"
+let str2 = "hitesh"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+ 2);
+console.log(1+"2");
+console.log("1"+ 2+2);
+console.log(1+2+"2");
+console.log(3+4 * 5%3);;
+console.log(+true;// 1 will be printed
+
+    let gameCounter = 100
+    ++gameCounter;
+    console.log(gameCounter);
+
+    // prefix :  (++ x) increment first then use 
+    // postfix:  (x++) used first, then incremented 
